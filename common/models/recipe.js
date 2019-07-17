15cebe969a44bb3ba165570cee482367fc67d6db
eyuel <eyuel.berga@yahoo.com>
2019-07-17 15:51:47 +0300

    modifications on token recipe and user

--- common/models/recipe.js
+++ common/models/recipe.js
@@ -1,5 +1,10 @@
 'use strict';
 
 module.exports = function(Recipe) {
-
+  Recipe.beforeRemote('create', function(ctx, instance, next) {
+    console.log('before create...add owner id');
+    // set creator to current user
+    ctx.args.data.ownerId = ctx.req.accessToken.userId;
+    next();
+  });
 };
